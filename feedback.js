92ec328901cce37d7963a49f5c805db87279f117
DiGureev <dianagureev@gmail.com>
2024-07-31 22:18:25 +0300

    feedback

---
+++ feedback.js
@@ -0,0 +1,4 @@
+// Cool project! I like that we can choose the product when we type in the search. Good idea. 
+// I love that you practised using API fetching and localStorage.
+
+// I just wish you use a database and server to make a project work, to practise with a bit. Maybe you could do login/logout and remember user's ingredients or favourite recipe. Also if you want you can play with styles a bit, now when you see all the recipes the styles are not stable :) 
